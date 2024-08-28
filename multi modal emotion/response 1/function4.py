1e10fe7f9ea36ebbec86bef3999c3375b6b11450
Krishnasridhar28 <115479869+Krishnasridhar28@users.noreply.github.com>
2024-08-28 20:57:23 +0530

    Update function4.py

--- multi modal emotion/response 1/function4.py
+++ multi modal emotion/response 1/function4.py
@@ -1,14 +1,14 @@
-import numpy as np
 from keras.models import load_model
-from utils.preprocessing import preprocess_speech
-from utils.datasets import load_speech_data
+from utils.preprocessing import preprocess_facial_expression
+from utils.datasets import load_facial_expression_data
 
-model = load_model('models/speech_model.h5')
+model = load_model('models/emotion_model.h5')
+detector = dlib.get_frontal_face_detector()
 
-def recognize_speech_emotion(audio_path):
-    mfccs = load_speech_data(audio_path)
-    preprocessed_mfccs = preprocess_speech(mfccs)
-    predictions = model.predict(preprocessed_mfccs)
+def recognize_facial_expression(image_path):
+    image = load_facial_expression_data(image_path)
+    preprocessed_image = preprocess_facial_expression(image)
+    predictions = model.predict(preprocessed_image)
     return predictions
 
-'voice recoginition'
+'''face recoginition'''
